5f6c4b37d6ff6852897c671a1e51abdda3e8f90a
tiltom <tilnak.tomas@gmail.com>
2021-09-08 17:42:56 +0200

    Add data attributes to the Select wallet type

--- packages/react-wallet/src/components/Item/index.tsx
+++ packages/react-wallet/src/components/Item/index.tsx
@@ -14,6 +14,7 @@ interface Props {
   linkHref?: string;
   small?: boolean;
   href?: string;
+  dataAttribute?: string;
 }
 
 export function Item(props: Props) {
@@ -23,6 +24,7 @@ export function Item(props: Props) {
         [style.container_big]: props.linkHref && props.linkTitle,
         [style.small]: props.small,
       })}
+      data-action-id={props.dataAttribute}
     >
       <button
         type='button'
